8827077d79fc2f7a813ee172a7bd2590a035b6f3
Daniel Hurtarte <dhurtart@uci.edu>
2025-11-05 19:55:09 -0800

    Set up event object using pydantic based model

--- backend/main.py
+++ backend/main.py
@@ -1,30 +1,19 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
-class Item(BaseModel):
-    text: str = None
-    is_done: bool = False
+class Event(BaseModel):
+    id:  int
+    name: str
+    host: str
+    time: str #ISO datetime
+    location: str
+    attendees: List[str] = []
 
-items = []
+events_db = []
 
-@app.get("/")
-def root():
-    return {"Hello": "World"}
-
-@app.post("/items")
+@app.post("/events")
 def create_item(item: Item):
-    items.append(item)
-    return items
-
-@app.get("/items")
-def list_items(limit: int = 10):
-    return items[0:limit]
-
-@app.get("/items/{item_id}")
-def get_item(item_id: int) -> Item:
-    if item_id < len(items):
-        return items[item_id]
-    else:
-        raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
+    pass
