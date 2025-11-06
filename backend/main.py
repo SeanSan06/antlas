200d6869e244072218210e9d27049ef86e7d978c
Lien Jabujab <lbjabujab58@gmail.com>
2025-11-06 00:18:08 -0800

    Fix syntax

--- backend/main.py
+++ backend/main.py
@@ -2,14 +2,14 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
-import _sqlite3
+import sqlite3
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
